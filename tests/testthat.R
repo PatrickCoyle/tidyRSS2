eed6405f1baf3b8bbedbca8a1da13bf9da5fab09
PatrickCoyle <patricktmc@gmail.com>
2024-08-17 13:13:18 -0500

    Metadata updates

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
 library(testthat)
-library(tidyRSS)
+library(tidyRSS2)
 
-test_check("tidyRSS")
+test_check("tidyRSS2")
--- tests/testthat/test-cfeed.R
+++ tests/testthat/test-cfeed.R
@@ -1,27 +1,25 @@
 test_that("Patrick Coyle's tidyRSS feed works", {
   expect_no_error(
-    myfeeds <- tribble(
+    myfeeds <- dplyr::tribble(
       ~source, ~url,
       "tidyverse", "https://www.tidyverse.org/blog/index.xml",
-      "rOpenSci", "https://ropensci.org/rbloggers/index.xml",
-      "brodrigues", "https://www.brodrigues.co/index.xml",
-      "nrennie", "https://nrennie.rbind.io/blog/index.xml"
+      "rOpenSci", "https://ropensci.org/rbloggers/index.xml"
     ) %>%
-      mutate(feed = url %>% map(
-        ~.x %>%
-          tidyRSS2::tidyfeed() %>%
-          mutate(item_pub_date = ymd(item_pub_date)) %>%
-          filter(item_pub_date >= today() - 60)
-      )) %>%
-      unnest(cols = c(feed)) %>%
-      arrange(desc(item_pub_date), source) %>%
-      mutate(
-        link = paste0('<a target=_blank href="',
-                      item_link,
-                      '">',
-                      item_title,
-                      '</a>')
-      ) %>%
-      select(item_pub_date, source, link)
+    dplyr::mutate(feed = url %>% purrr::map(
+      ~.x %>%
+      tidyRSS2::tidyfeed() %>%
+      dplyr::mutate(item_pub_date = lubridate::ymd(item_pub_date)) %>%
+      dplyr::filter(item_pub_date >= lubridate::today() - 60)
+    )) %>%
+    tidyr::unnest(cols = c(feed)) %>%
+    dplyr::arrange(dplyr::desc(item_pub_date), source) %>%
+    dplyr::mutate(
+      link = paste0('<a target=_blank href="',
+      item_link,
+      '">',
+      item_title,
+      '</a>')
+    ) %>%
+    dplyr::select(item_pub_date, source, link)
   )
 })
